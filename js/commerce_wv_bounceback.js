bd5cc1cffec1b5af2267b064eef41aa7179b08f0
Gaby Molina Goigoux <gaby.molina.goigoux@gmail.com>
2016-11-18 12:07:29 +0100

    Remove unnecesary console log

--- js/commerce_wv_bounceback.js
+++ js/commerce_wv_bounceback.js
@@ -2,7 +2,6 @@
     Drupal.behaviors.CommerceWvBounceback = {
         attach: function (context, settings) {
 		$("input[type=radio][name='line_item_fields[field_wv_bb_predet_texts][und]'").change(function() {
-			console.log('Hola');
 			$('#edit-line-item-fields-field-wv-bounceback-text-und-0-value').val($(this).parent().text());
 		});
 
