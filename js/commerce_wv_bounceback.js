dcf73bde8ff65f8f41a484b36f1a5c32c1799cfa
Gaby Molina Goigoux <gaby.molina.goigoux@gmail.com>
2016-11-18 12:03:51 +0100

    Fix selector for bounceback predet texts
    See https://www.pivotaltracker.com/story/show/134571741 for more info.

--- js/commerce_wv_bounceback.js
+++ js/commerce_wv_bounceback.js
@@ -1,7 +1,8 @@
 (function ($) {
     Drupal.behaviors.CommerceWvBounceback = {
         attach: function (context, settings) {
-		$("input[type=radio][name='line_item_fields[field_wv_bb_line_predet_texts][und]'").change(function() {
+		$("input[type=radio][name='line_item_fields[field_wv_bb_predet_texts][und]'").change(function() {
+			console.log('Hola');
 			$('#edit-line-item-fields-field-wv-bounceback-text-und-0-value').val($(this).parent().text());
 		});
 
