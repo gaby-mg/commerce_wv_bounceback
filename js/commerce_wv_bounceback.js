05b5c32272b03eb86fa19faea6f18b064563b7eb
Gaby Molina Goigoux <gaby.molina.goigoux@gmail.com>
2016-11-20 14:37:15 +0100

    Change Add to Cart text

--- js/commerce_wv_bounceback.js
+++ js/commerce_wv_bounceback.js
@@ -1,11 +1,13 @@
 (function ($) {
     Drupal.behaviors.CommerceWvBounceback = {
         attach: function (context, settings) {
-		$("input[type=radio][name='line_item_fields[field_wv_bb_predet_texts][und]'").change(function() {
-			$('#edit-line-item-fields-field-wv-bounceback-text-und-0-value').val($(this).parent().text());
-		});
+			$("input[type=radio][name='line_item_fields[field_wv_bb_predet_texts][und]'").change(function() {
+				$('#edit-line-item-fields-field-wv-bounceback-text-und-0-value').val($(this).parent().text());
+			});
 
-		$('.commerce-add-to-cart .form-radio[value=_none]').parent().hide();
-	}
+			$('.commerce-add-to-cart .form-radio[value=_none]').parent().hide();
+
+			$('.field-name-field-wv-bounceback-reference .form-submit').text('Continuar');
+		}
     };
 })(jQuery);
